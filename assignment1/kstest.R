dbe99b0217984f5d9bff44a313fea404ff45c94b
Puck Mulders <pjammulders@gmail.com>
2016-05-21 22:12:16 +0200

    dataset weggehaald, hoeft niet in verslag, geprobeerd een verdeling te maken tussen methods en results. Voor en methods en results t-test uitgewerkt, log normal shapiro uitgewerkt,  homogeniety oid tests uitgewerkt en ik heb ook al die kormogov test gemaakt in R

--- assignment1/kstest.R
+++ assignment1/kstest.R
@@ -1,6 +1,7 @@
 library(sas7bdat)
 library(outliers)
 library(plyr)
+library(nortest)
 
 dataset <- read.sas7bdat("/Users/puckmulders/Documents/Studie/Statistics for Big Data - poging 2/assignment1.sas7bdat")
 
@@ -12,21 +13,13 @@ data <- dataset[-which(data$BATCH=="B0"),]
 
 batches = unique(data$BATCH);
 
-ttestvalues = list();
-variance = list()
+kstestvalues = list();
+adtestvalues = list()
 vartestvalues = list()
 
 for (batch in batches) {
-  variance[batch] <- var(data[data$BATCH==batch,]$OUTCOME)
   Bi = data[data$BATCH == batch,]$OUTCOME
-  ttestvalues[batch] <- t.test(batch_0$OUTCOME,Bi,var.equal = FALSE)$p.value;
-  vartestvalues[batch] <- var.test(batch_0$OUTCOME,Bi)$p.value
+  kstestvalues[batch] <- ks.test(batch_0$OUTCOME,Bi,var.equal = FALSE)$p.value;
 }
 
-variance_batch_0 <- var(batch_0$OUTCOME)
-
-qqnorm(batch_0$OUTCOME)
-qqline(batch_0$OUTCOME)
-
-bartlett.test(dataset$OUTCOME~dataset$BATCH, data = dataset)
 
