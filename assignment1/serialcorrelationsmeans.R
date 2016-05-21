b7595facc239e1ef9118f32b352a8eb86b1c0ea6
Puck Mulders <pjammulders@gmail.com>
2016-05-21 23:12:59 +0200

    serial correlations
    om te kijken of de batches niet random verdeeld zijn. Heb de mean
    gepakt van elke batch en daarop heb ik die test gepakt -> je verwacht
    dat het de outcome steeds hoger wordt

---
+++ assignment1/serialcorrelationsmeans.R
@@ -0,0 +1,30 @@
+library(sas7bdat)
+library(outliers)
+library(plyr)
+library(nortest)
+library(goftest)
+library(e1071)
+library(EnvStats)
+
+dataset <- read.sas7bdat("/Users/puckmulders/Documents/Studie/Statistics for Big Data - poging 2/assignment1.sas7bdat")
+
+data$OUTCOME <- log(data$OUTCOME)
+
+batch_0 <- dataset[which(dataset$BATCH=="B0"),]
+data <- dataset[-c(91),]
+
+batches = unique(data$BATCH);
+
+kstestvalues = list();
+means = list()
+vartestvalues = list()
+
+for (batch in batches) {
+  Bi = data[data$BATCH == batch,]$OUTCOME
+  means[batch] <- mean(Bi)
+  
+}
+
+means <- as.numeric(means)
+
+serialCorrelationTest(means)
